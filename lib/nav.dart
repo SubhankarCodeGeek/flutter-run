d8b31c17c0c48223ff97f138b6b67625b9a1c79e
“Subhankar <quaser143@gmail.com>
2023-05-08 18:12:07 +0530

    ~ working bottom navigation with multiple backStack.

---
+++ lib/nav.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Nav extends StatefulWidget {
+  const Nav({Key? key}) : super(key: key);
+
+  @override
+  State<Nav> createState() => _NavState();
+}
+
+class _NavState extends State<Nav> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Bottom App Bar"),
+      ),
+      body: const Text("HomeScreen"),
+    );
+  }
+}
